14599808396c5ab7d67732aa8f1aa6b4ec4b672a
Maciej Madejczyk <48794984+MadejaMaciej@users.noreply.github.com>
2024-05-04 16:20:49 +0200

    feat: 🎸 Bundle assets using capacitor not web
    ✅ Closes: #172

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,12 +3,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.lostdutchmanmine.app",
   appName: "lost-dutchman-mine",
-  webDir: "",
-  server: {
-    hostname: "play.lostmine.cc",
-    androidScheme: "https",
-    url: "https://play.lostmine.cc",
-  },
+  webDir: ".webpack/renderer",
 };
 
 export default config;
