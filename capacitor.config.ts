ea14cd62c1633f20247e8d8843feb95185b54ca4
Maciej Madejczyk <48794984+MadejaMaciej@users.noreply.github.com>
2024-05-05 20:56:08 +0200

    feat: 🎸 Set app's icon for the mobile version
    ✅ Closes: #117

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.lostdutchmanmine.app",
-  appName: "lost-dutchman-mine",
+  appName: "Lost Dutchman Mine",
   webDir: ".webpack/renderer",
 };
 
