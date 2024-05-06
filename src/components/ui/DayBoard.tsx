62f71cbb9728bc434ea9840513787325d8f9c01d
Maciej Madejczyk <48794984+MadejaMaciej@users.noreply.github.com>
2024-05-06 19:55:32 +0200

    fix: 🐛 Calendar's label is mis-aligned
    ✅ Closes: #147

--- src/components/ui/DayBoard.tsx
+++ src/components/ui/DayBoard.tsx
@@ -2,19 +2,8 @@ import React from "react";
 
 function DayBoard() {
   return (
-    <div
-      style={{
-        background: "#503000",
-      }}
-    >
-      <h3
-        style={{
-          textAlign: "center",
-          margin: 0,
-        }}
-      >
-        MONDAY
-      </h3>
+    <div className="day-board">
+      <h3 className="day-board-text">MONDAY</h3>
     </div>
   );
 }
